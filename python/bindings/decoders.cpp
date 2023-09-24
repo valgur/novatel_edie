fe2086e5381eea6dad145bb09c93853503449b8f
Martin Valgur <martin.valgur@gmail.com>
2023-09-24 17:13:09 +0300

    Set up project skeleton for nanobind + cibuildwheel

---
+++ python/bindings/decoders.cpp
@@ -0,0 +1,7 @@
+#include <nanobind/nanobind.h>
+
+namespace nb = nanobind;
+
+NB_MODULE(decoders_bindings, m) {
+
+}
---
+++ python/bindings/hw_interface.cpp
@@ -0,0 +1,7 @@
+#include <nanobind/nanobind.h>
+
+namespace nb = nanobind;
+
+NB_MODULE(hw_interface_bindings, m) {
+
+}
