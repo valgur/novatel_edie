b63865651d190bea21fad7fa742d47c4dd1d55a1
Martin Valgur <martin.valgur@gmail.com>
2023-10-04 18:32:16 +0300

    Cover jsonreader.hpp with bindings

--- python/bindings/common/jsonreader.cpp
+++ python/bindings/common/jsonreader.cpp
@@ -1,5 +1,7 @@
 #include "common/jsonreader.hpp"
 
+#include <nanobind/stl/unordered_map.h>
+
 #include "bindings_core.hpp"
 
 namespace nb = nanobind;
@@ -8,6 +10,147 @@ using namespace novatel::edie;
 
 void init_common_jsonreader(nb::module_& m)
 {
+    nb::exception<JsonReaderFailure>(m, "JsonReaderFailure");
+
+    nb::enum_<CONVERSION_STRING>(m, "CONVERSION_STRING", "Conversion string represented as an enum.")
+        .value("d", CONVERSION_STRING::d)
+        .value("ld", CONVERSION_STRING::ld)
+        .value("hd", CONVERSION_STRING::hd)
+        .value("lld", CONVERSION_STRING::lld)
+        .value("u", CONVERSION_STRING::u)
+        .value("lu", CONVERSION_STRING::lu)
+        .value("hu", CONVERSION_STRING::hu)
+        .value("llu", CONVERSION_STRING::llu)
+        .value("c", CONVERSION_STRING::c)
+        .value("uc", CONVERSION_STRING::uc)
+        .value("Z", CONVERSION_STRING::Z)
+        .value("B", CONVERSION_STRING::B)
+        .value("UB", CONVERSION_STRING::UB)
+        .value("XB", CONVERSION_STRING::XB)
+        .value("x", CONVERSION_STRING::x)
+        .value("X", CONVERSION_STRING::X)
+        .value("lx", CONVERSION_STRING::lx)
+        .value("ucb", CONVERSION_STRING::ucb)
+        .value("f", CONVERSION_STRING::f)
+        .value("lf", CONVERSION_STRING::lf)
+        .value("k", CONVERSION_STRING::k)
+        .value("lk", CONVERSION_STRING::lk)
+        .value("e", CONVERSION_STRING::e)
+        .value("le", CONVERSION_STRING::le)
+        .value("g", CONVERSION_STRING::g)
+        .value("P", CONVERSION_STRING::P)
+        .value("s", CONVERSION_STRING::s)
+        .value("S", CONVERSION_STRING::S)
+        .value("id", CONVERSION_STRING::id, "SATELLITEID")
+        .value("R", CONVERSION_STRING::R, "RXCONFIG")
+        .value("m", CONVERSION_STRING::m, "MessageName")
+        .value("T", CONVERSION_STRING::T, "GPSTime value")
+        .value("UNKNOWN", CONVERSION_STRING::UNKNOWN);
+
+    m.attr("str_to_CONVERSION_STRING") = ConversionStringEnumLookup;
+
+    nb::enum_<DATA_TYPE_NAME>(m, "DATA_TYPE_NAME", "Data type name string represented as an enum")
+        .value("BOOL", DATA_TYPE_NAME::BOOL)
+        .value("HEXBYTE", DATA_TYPE_NAME::HEXBYTE)
+        .value("CHAR", DATA_TYPE_NAME::CHAR)
+        .value("UCHAR", DATA_TYPE_NAME::UCHAR)
+        .value("SHORT", DATA_TYPE_NAME::SHORT)
+        .value("USHORT", DATA_TYPE_NAME::USHORT)
+        .value("INT", DATA_TYPE_NAME::INT)
+        .value("UINT", DATA_TYPE_NAME::UINT)
+        .value("LONG", DATA_TYPE_NAME::LONG)
+        .value("ULONG", DATA_TYPE_NAME::ULONG)
+        .value("LONGLONG", DATA_TYPE_NAME::LONGLONG)
+        .value("ULONGLONG", DATA_TYPE_NAME::ULONGLONG)
+        .value("FLOAT", DATA_TYPE_NAME::FLOAT)
+        .value("DOUBLE", DATA_TYPE_NAME::DOUBLE)
+        .value("EMBEDDED_HEADER", DATA_TYPE_NAME::EMBEDDED_HEADER)
+        .value("EMBEDDED_BODY", DATA_TYPE_NAME::EMBEDDED_BODY)
+        .value("SATELLITEID", DATA_TYPE_NAME::SATELLITEID)
+        .value("UNKNOWN", DATA_TYPE_NAME::UNKNOWN);
+
+    m.attr("str_to_DATA_TYPE") = DataTypeEnumLookup;
+
+    nb::enum_<FIELD_TYPE>(m, "FIELD_TYPE", "Field type string represented as an enum.")
+        .value("SIMPLE", FIELD_TYPE::SIMPLE)
+        .value("ENUM", FIELD_TYPE::ENUM)
+        .value("BITFIELD", FIELD_TYPE::BITFIELD)
+        .value("FIXED_LENGTH_ARRAY", FIELD_TYPE::FIXED_LENGTH_ARRAY)
+        .value("VARIABLE_LENGTH_ARRAY", FIELD_TYPE::VARIABLE_LENGTH_ARRAY)
+        .value("STRING", FIELD_TYPE::STRING)
+        .value("FIELD_ARRAY", FIELD_TYPE::FIELD_ARRAY)
+        .value("RESPONSE_ID", FIELD_TYPE::RESPONSE_ID)
+        .value("RESPONSE_STR", FIELD_TYPE::RESPONSE_STR)
+        .value("RXCONFIG_HEADER", FIELD_TYPE::RXCONFIG_HEADER)
+        .value("RXCONFIG_BODY", FIELD_TYPE::RXCONFIG_BODY)
+        .value("UNKNOWN", FIELD_TYPE::UNKNOWN);
+
+    m.attr("str_to_FIELD_TYPE") = FieldTypeEnumLookup;
+
+    nb::class_<EnumDataType>(m, "EnumDataType", "Enum Data Type representing contents of UI DB")
+        .def(nb::init<>())
+        .def_rw("value", &EnumDataType::value)
+        .def_rw("name", &EnumDataType::name)
+        .def_rw("description", &EnumDataType::description);
+
+    nb::class_<EnumDefinition>(m, "EnumDefinition", "Enum Definition representing contents of UI DB")
+        .def(nb::init<>())
+        .def_rw("id", &EnumDefinition::_id)
+        .def_rw("name", &EnumDefinition::name)
+        .def_rw("enumerators", &EnumDefinition::enumerators);
+
+    nb::class_<BaseDataType>(m, "BaseDataType", "Struct containing basic elements of data type fields in the UI DB")
+        .def(nb::init<>())
+        .def_rw("name", &BaseDataType::name)
+        .def_rw("length", &BaseDataType::length)
+        .def_rw("description", &BaseDataType::description);
+
+    nb::class_<SimpleDataType, BaseDataType>(m, "SimpleDataType", "Struct containing elements of simple data type fields in the UI DB")
+        .def(nb::init<>())
+        .def_rw("enums", &SimpleDataType::enums);
+
+    nb::class_<BaseField>(m, "BaseField", "Struct containing elements of basic fields in the UI DB")
+        .def(nb::init<>())
+        .def_rw("name", &BaseField::name)
+        .def_rw("type", &BaseField::type)
+        .def_rw("description", &BaseField::description)
+        .def_rw("conversion", &BaseField::conversion)
+        .def_rw("conversion_stripped", &BaseField::conversionStripped)
+        .def_rw("conversion_before_point", &BaseField::conversionBeforePoint)
+        .def_rw("conversion_after_point", &BaseField::conversionAfterPoint)
+        .def_rw("data_type", &BaseField::dataType)
+        .def("clone", &BaseField::clone)
+        .def("set_conversion", &BaseField::setConversion, "conversion"_a)
+        .def("parse_conversion", &BaseField::parseConversion, "str_stripped_conversion_string"_a, "before_point"_a, "after_point"_a);
+
+    nb::class_<EnumField, BaseField>(m, "EnumField", "Struct containing elements of enum fields in the UI DB")
+        .def(nb::init<>())
+        .def_rw("enum_id", &EnumField::enumID)
+        .def_rw("enum_def", &EnumField::enumDef)
+        .def_rw("length", &EnumField::length)
+        .def("clone", &EnumField::clone);
+
+    nb::class_<ArrayField, BaseField>(m, "ArrayField", "Struct containing elements of array fields in the UI DB")
+        .def(nb::init<>())
+        .def_rw("array_length", &ArrayField::arrayLength)
+        .def("clone", &ArrayField::clone);
+
+    nb::class_<FieldArrayField, BaseField>(m, "FieldArrayField", "Struct containing elements of field array fields in the UI DB")
+        .def(nb::init<>())
+        .def_rw("array_length", &FieldArrayField::arrayLength)
+        .def_rw("field_size", &FieldArrayField::fieldSize)
+        .def_rw("fields", &FieldArrayField::fields)
+        .def("clone", &FieldArrayField::clone);
+
+    nb::class_<MessageDefinition>(m, "MessageDefinition", "Struct containing elements of message definitions in the UI DB")
+        .def(nb::init<>())
+        .def_rw("_id", &MessageDefinition::_id)
+        .def_rw("log_id", &MessageDefinition::logID)
+        .def_rw("name", &MessageDefinition::name)
+        .def_rw("description", &MessageDefinition::description)
+        .def_rw("fields", &MessageDefinition::fields)
+        .def_rw("latest_message_crc", &MessageDefinition::latestMessageCrc);
+
     nb::class_<JsonReader>(m, "JsonReader")
         .def(nb::init<>())
         .def(
--- python/bindings/novatel/message_decoder.cpp
+++ python/bindings/novatel/message_decoder.cpp
@@ -1,13 +1,30 @@
 #include "decoders/novatel/message_decoder.hpp"
 
+#include <nanobind/stl/bind_vector.h>
+#include <nanobind/stl/variant.h>
+
 #include "bindings_core.hpp"
 
 namespace nb = nanobind;
 using namespace nb::literals;
 using namespace novatel::edie;
+using namespace novatel::edie::oem;
+
+NB_MAKE_OPAQUE(oem::IntermediateMessage);
+// NB_MAKE_OPAQUE(oem::MsgFieldsVector);
 
 void init_novatel_message_decoder(nb::module_& m)
 {
+
+    nb::bind_vector<oem::IntermediateMessage>(m, "IntermediateMessage");
+    //   nb::bind_vector<oem::MsgFieldsVector>(m, "MsgFieldsVector");
+
+    nb::class_<oem::FieldContainer>(m, "FieldContainer")
+        //      .def(nb::init<std::variant<container_types>, const novatel::edie::BaseField*>(), "field_value"_a, "field_def"_a)
+        //      .def(nb::init<const oem::FieldContainer&>())
+        .def_rw("field_value", &oem::FieldContainer::field_value)
+        .def_rw("field_def", &oem::FieldContainer::field_def);
+
     nb::class_<oem::MessageDecoder>(m, "MessageDecoder")
         .def(nb::init<JsonReader*>(), "json_db"_a)
         .def("load_json_db", &oem::MessageDecoder::LoadJsonDb, "json_db"_a)
