e14d5b636b4ae8c89f70c6a69c9ef67e5f5a81dd
Martin Valgur <martin.valgur@gmail.com>
2023-10-04 12:01:15 +0300

    Convert test_rangecmp.py

--- python/bindings/novatel/range_decompressor.cpp
+++ python/bindings/novatel/range_decompressor.cpp
@@ -1,5 +1,6 @@
 #include "decoders/novatel/rangecmp/range_decompressor.hpp"
 #include "bindings_core.h"
+#include <cstring>
 
 namespace nb = nanobind;
 using namespace nb::literals;
@@ -13,8 +14,13 @@ void init_novatel_range_decompressor(nb::module_& m)
       .def_prop_ro("logger", &oem::Encoder::GetLogger)
       .def("reset", &oem::RangeDecompressor::Reset)
       .def("decompress", [](oem::RangeDecompressor& self, nb::bytes data, oem::MetaDataStruct& metadata,
-                            ENCODEFORMAT encode_format) {
-         STATUS status = self.Decompress((unsigned char*) data.c_str(), data.size(), metadata, encode_format);
-         return status;
+                            ENCODEFORMAT encode_format) -> nb::object {
+         if (data.size() > MESSAGE_SIZE_MAX)
+            return nb::make_tuple(STATUS::BUFFER_FULL, nb::none());
+         char buffer[MESSAGE_SIZE_MAX];
+         uint32_t buf_size = MESSAGE_SIZE_MAX;
+         memcpy(buffer, data.c_str(), data.size());
+         STATUS status = self.Decompress((unsigned char*) buffer, buf_size, metadata, encode_format);
+         return nb::make_tuple(status, nb::bytes(buffer, metadata.uiLength));
       }, "data"_a, "metadata"_a, "encode_format"_a = ENCODEFORMAT::UNSPECIFIED);
 }
