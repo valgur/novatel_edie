5ed2b0a937bfb06fd03f93eda645f4b854190fdb
Martin Valgur <martin.valgur@gmail.com>
2023-10-10 09:54:49 +0300

    Add bindings for NExcept

---
+++ python/bindings/common/nexcept.cpp
@@ -0,0 +1,20 @@
+#include "edie/decoders/common/nexcept.h"
+
+#include "bindings_core.hpp"
+
+namespace nb = nanobind;
+using namespace nb::literals;
+
+void init_common_nexcept(nb::module_& m)
+{
+    nb::register_exception_translator([](const std::exception_ptr& p, void*) {
+        try
+        {
+            std::rethrow_exception(p);
+        }
+        catch (const NExcept& e)
+        {
+            PyErr_SetString(PyExc_OSError, e.buffer);
+        }
+    });
+}
